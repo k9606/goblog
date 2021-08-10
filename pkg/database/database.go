fffbc0597611daf58dc198a9901f84d57de234db
bishengfei@126.com <19940611hqr>
2021-08-10 18:01:33 +0800

    重构至 PagesController

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -24,7 +24,7 @@ func initDB() {
 	// 设置数据库连接信息
 	config := mysql.Config{
 		User:                 "root",
-		Passwd:               "secret",
+		Passwd:               "qqqqqq",
 		Addr:                 "127.0.0.1:3306",
 		Net:                  "tcp",
 		DBName:               "goblog",
